5d49fe1d9cee25482217c7cb1d55a9aa931c332a
m9dnrs <m9dnrs8608@daum.net>
2022-05-07 21:33:18 +0900

    styles init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
 import Router from './Router';
+import reset from 'styled-reset';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    font-family: 'Source Sans Pro', sans-serif;
+    background-color:${(props) => props.theme.bgColor};
+    color: ${(props) => props.theme.textColor};
+  }
+  a {
+    text-decoration: none;
+  }
+`;
+
 function App() {
-	return <Router />;
+	return (
+		<>
+			<GlobalStyle />
+			<Router />
+		</>
+	);
 }
 
 export default App;
