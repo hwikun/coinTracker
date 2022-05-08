0bf41bcbbd2915b54ee19eb2299362e30b2cd226
m9dnrs <m9dnrs8608@daum.net>
2022-05-08 09:44:00 +0900

    get data for Coin components

--- src/route/Coin.tsx
+++ src/route/Coin.tsx
@@ -1,11 +1,35 @@
 import { useState, useEffect } from 'react';
-import { useParams, useLocation } from 'react-router';
+import { useParams, useLocation, Routes, Route } from 'react-router';
 
 import styled from 'styled-components';
+import Chart from './Chart';
+import Price from './Price';
 
 const Title = styled.h1`
 	font-size: 48px;
 	color: ${(props) => props.theme.accentColor};
+`;
+
+const Overview = styled.div`
+	display: flex;
+	justify-content: space-between;
+	background-color: rgba(0, 0, 0, 0.5);
+	padding: 10px 20px;
+	border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	span:first-child {
+		font-size: 10px;
+		font-weight: 400;
+		text-transform: uppercase;
+		margin-bottom: 5px;
+	}
+`;
+const Description = styled.p`
+	margin: 20px 0px;
 `;
 
 const Loader = styled.span`
@@ -103,15 +127,50 @@ function Coin() {
 			).json();
 			setInfo(infoData);
 			setPriceInfo(priceData);
+			setLoading(false);
 		})();
-	}, []);
+	}, [coinId]);
 
 	return (
 		<Container>
 			<Header>
-				<Title>{name || 'Loading...'}</Title>
+				<Title>{name ? name : loading ? 'Loading...' : info?.name}</Title>
 			</Header>
-			{loading ? <Loader>Loading...</Loader> : null}
+			{loading ? (
+				<Loader>Loading...</Loader>
+			) : (
+				<>
+					<Overview>
+						<OverviewItem>
+							<span>Rank:</span>
+							<span>{info?.rank}</span>
+						</OverviewItem>
+						<OverviewItem>
+							<span>Symbol:</span>
+							<span>{info?.symbol}</span>
+						</OverviewItem>
+						<OverviewItem>
+							<span>Open Source:</span>
+							<span>{info?.open_source ? 'Yes' : 'No'}</span>
+						</OverviewItem>
+					</Overview>
+					<Description>{info?.description}</Description>
+					<Overview>
+						<OverviewItem>
+							<span>Total Supply:</span>
+							<span>{priceInfo?.total_supply}</span>
+						</OverviewItem>
+						<OverviewItem>
+							<span>Max Supply:</span>
+							<span>{priceInfo?.max_supply}</span>
+						</OverviewItem>
+					</Overview>
+					<Routes>
+						<Route path={`/${coinId}/price`} element={<Price />}></Route>
+						<Route path={`/${coinId}/chart`} element={<Chart />}></Route>
+					</Routes>
+				</>
+			)}
 		</Container>
 	);
 }
