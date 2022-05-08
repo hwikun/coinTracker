7ad92f76e7eb2718a7b37b3da9ff8fdebf8df04d
m9dnrs <m9dnrs8608@daum.net>
2022-05-08 20:40:01 +0900

    create chart and price tab

--- src/route/Coin.tsx
+++ src/route/Coin.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams, useLocation, Routes, Route } from 'react-router';
+import { Link, useMatch } from 'react-router-dom';
 
 import styled from 'styled-components';
 import Chart from './Chart';
@@ -48,6 +49,28 @@ const Header = styled.header`
 	display: flex;
 	justify-content: center;
 	align-items: center;
+`;
+
+const Tabs = styled.div`
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+	margin: 25px 0px;
+	gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+	text-align: center;
+	text-transform: uppercase;
+	font-size: 12px;
+	font-weight: 400;
+	background-color: rgba(0, 0, 0, 0.5);
+	padding: 7px 0px;
+	border-radius: 10px;
+	color: ${(props) =>
+		props.isActive ? props.theme.accentColor : props.theme.textColor};
+	a {
+		display: block;
+	}
 `;
 
 interface RouteState {
@@ -112,6 +135,8 @@ interface PriceData {
 function Coin() {
 	const [loading, setLoading] = useState(true);
 	const { coinId } = useParams();
+	const priceMatch = useMatch('/:coinId/price');
+	const chartMatch = useMatch('/:coinId/chart');
 	const {
 		state: { name },
 	} = useLocation() as RouteState;
@@ -165,9 +190,17 @@ function Coin() {
 							<span>{priceInfo?.max_supply}</span>
 						</OverviewItem>
 					</Overview>
+					<Tabs>
+						<Tab isActive={chartMatch !== null}>
+							<Link to={`/${coinId}/chart`}>Chart</Link>
+						</Tab>
+						<Tab isActive={priceMatch !== null}>
+							<Link to={`/${coinId}/Price`}>Price</Link>
+						</Tab>
+					</Tabs>
 					<Routes>
-						<Route path={`/${coinId}/price`} element={<Price />}></Route>
-						<Route path={`/${coinId}/chart`} element={<Chart />}></Route>
+						<Route path={`/:coinId/price`} element={<Price />}></Route>
+						<Route path={`/:coinId/chart`} element={<Chart />}></Route>
 					</Routes>
 				</>
 			)}
