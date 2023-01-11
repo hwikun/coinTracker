b0b413b7dcb7c7407bf9b24c651a721c36fdd816
Hwikun <leah8608@gmail.com>
2023-01-11 13:18:01 +0900

    debug chart api

--- src/api.ts
+++ src/api.ts
@@ -1,18 +1,18 @@
-const BASE_URL = 'https://api.coinpaprika.com/v1';
+const BASE_URL = "https://api.coinpaprika.com/v1";
 
 export function fetchCoins() {
-	return fetch(`${BASE_URL}/coins`).then((res) => res.json());
+    return fetch(`${BASE_URL}/coins`).then(res => res.json());
 }
 export function fetchCoinInfo(coinId: string) {
-	return fetch(`${BASE_URL}/coins/${coinId}`).then((res) => res.json());
+    return fetch(`${BASE_URL}/coins/${coinId}`).then(res => res.json());
 }
 export function fetchCoinTickers(coinId: string) {
-	return fetch(`${BASE_URL}/tickers/${coinId}`).then((res) => res.json());
+    return fetch(`${BASE_URL}/tickers/${coinId}`).then(res => res.json());
 }
 export function fetchCoinHistory(coinId: string) {
-	const endDate = Math.floor(Date.now() / 1000);
-	const startDate = endDate - 60 * 60 * 24 * 7 * 2;
-	return fetch(
-		`${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`,
-	).then((res) => res.json());
+    const endDate = Math.floor(Date.now() / 1000);
+    const startDate = endDate - 60 * 60 * 23;
+    return fetch(
+        `${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`,
+    ).then(res => res.json());
 }
